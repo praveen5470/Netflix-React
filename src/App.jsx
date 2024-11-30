4217bb96ed36324aa4e9e352c296337af33d5c29
Kukudala Praveen Kumar Reddy <67887583+praveen5470@users.noreply.github.com>
2024-11-29 18:06:05 -0600

    Normalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <>
+    <AuthContextProvider>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/account' element={<ProtectedRoute><Account/></ProtectedRoute>}></Route>
+      </Routes>
+    </AuthContextProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER,
+  // appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyCC9_OawTa8NFRvTK3l5T0Er7c7UIhk4D0",
+  authDomain: "netflix-clone-b8a53.firebaseapp.com",
+  projectId: "netflix-clone-b8a53",
+  storageBucket: "netflix-clone-b8a53.firebasestorage.app",
+  messagingSenderId: "1076780781970",
+  appId: "1:1076780781970:web:eaa2447ad48534c77db14b"
+};
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
