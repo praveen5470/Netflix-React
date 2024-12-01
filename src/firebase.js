baed3ee6e3b8ab22e86c39dc3141225592fbec45
Kukudala Praveen Kumar Reddy <67887583+praveen5470@users.noreply.github.com>
2024-12-01 13:02:03 -0600

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -10,18 +10,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER,
-  // appId: process.env.REACT_APP_APP_ID
-  apiKey: "AIzaSyCC9_OawTa8NFRvTK3l5T0Er7c7UIhk4D0",
-  authDomain: "netflix-clone-b8a53.firebaseapp.com",
-  projectId: "netflix-clone-b8a53",
-  storageBucket: "netflix-clone-b8a53.firebasestorage.app",
-  messagingSenderId: "1076780781970",
-  appId: "1:1076780781970:web:eaa2447ad48534c77db14b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER,
+  appId: process.env.REACT_APP_APP_ID
+  // apiKey: "AIzaSyCC9_OawTa8NFRvTK3l5T0Er7c7UIhk4D0",
+  // authDomain: "netflix-clone-b8a53.firebaseapp.com",
+  // projectId: "netflix-clone-b8a53",
+  // storageBucket: "netflix-clone-b8a53.firebasestorage.app",
+  // messagingSenderId: "1076780781970",
+  // appId: "1:1076780781970:web:eaa2447ad48534c77db14b"
 };
 console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
